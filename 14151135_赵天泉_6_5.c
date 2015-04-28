adfa6e970ce22879794419e170fcb33673fca7ed
2015CLanguage <buaa_2015_c@163.com>
2015-04-28 15:20:14 +0800

    Create 14151135_赵天泉_6_5.c

---
+++ 14151135_赵天泉_6_5.c
@@ -0,0 +1,27 @@
+# include<stdio.h>
+int main()
+{ 
+	int a[5],i,t;
+	printf("请输入五个整数：\n");
+
+	for(i=0;i<=4;i++)
+		scanf("%d",&a[i]);
+	printf("正序为：");
+	for(i=0;i<=4;i++)
+		printf("%5d",a[i]);
+	printf("\n");
+	for(i=0;i<=2;i++)
+	{
+		t=a[i];
+		a[i]=a[4-i];
+		a[4-i]=t;
+	}
+	printf("逆序为：");
+	for(i=0;i<=4;i++)
+		printf("%5d",a[i]);
+
+	printf("\n");
+	
+	return 0;
+
+}
