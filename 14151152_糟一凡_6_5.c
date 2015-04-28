00bcd29523de258cbcaab4f2ae742111bc63150f
2015CLanguage <buaa_2015_c@163.com>
2015-04-28 19:39:03 +0800

    Create 14151152_糟一凡_6_5.c

---
+++ 14151152_糟一凡_6_5.c
@@ -0,0 +1,28 @@
+#include"stdio.h"
+int main()
+{
+	int m,n,i,j,t;
+	float a[100],b[100];
+	printf("请输入数组元素个数：");
+	scanf("%d",&t);
+	printf("按顺序输入原数组：\n");
+	for(m=0;m<t;m++)
+	{
+		scanf("%f",&a[m]);
+	}
+    for(i=0;i<t;i++)
+	{
+		b[i]=a[i];
+	}
+	for(j=0;j<t;j++)
+	{
+		a[j]=b[t-j-1];
+	}
+    printf("数组逆序：");
+	for(n=0;n<t;n++)
+	{
+		printf("%f ",a[n]);
+	}
+	printf("\n");
+	return 0;
+}
