8626a82d73ac01f65c3a61417e55af602b01ff61
2015CLanguage <buaa_2015_c@163.com>
2015-04-28 15:19:05 +0800

    Create 14151204_周杨梓_6_5.c

--- 14151204_周杨梓_6_5.c
+++ 14151204_周杨梓_6_5.c
@@ -11,7 +11,7 @@ int main()
 	printf("您输入的数组为:");
 	for(i=1;i<=5;i++)
 	{
-		printf("%2d",a[i]);
+		printf("%d ",a[i]);
 	}
 	printf("\n");
 	for(i=1;i<=5/2;i++)
@@ -23,7 +23,7 @@ int main()
 	printf("逆序存放后的数组为:");
 	for(i=1;i<=5;i++)
 	{
-		printf("%2d",a[i]);
+		printf("%d ",a[i]);
 	}
 	printf("\n");
 	return 0;
