503b2e7889a820570663acc216a78e244be4d775
2015CLanguage <buaa_2015_c@163.com>
2015-04-28 14:27:21 +0800

    Create 14151164_张鹏_6_2.c

---
+++ 14151164_张鹏_6_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a[10];
+	int i,b,c;
+	for (i=0;i<10;i++)
+		scanf("%d",&a[i]);
+	printf("\n");
+	for (b=0;b<9;b++)
+		for(i=0;i<9-b;i++)
+			if(a[i+1]<a[i])
+			{c=a[i];
+			a[i]=a[i+1];
+			a[i+1]=c;
+			}
+			printf("排序后:\n");
+				for(i=0;i<10;i++)
+					printf("%7d",a[i]);
+				printf("\n");
+				return 0;
+}
