197c20fe2d86e47b9efaaae9b5d8156fe8ac2bdb
2015CLanguage <buaa_2015_c@163.com>
2015-04-28 15:23:16 +0800

    Create 14151204_周杨梓_6_9.c

---
+++ 14151204_周杨梓_6_9.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+    int a[16],top,i,n,flag,bot,distance;
+	printf("请按由大到小的顺序输入15个整数");
+	for(i=1;i<=15;i++)
+	{
+		printf("请输入第%d个整数:",i);
+		scanf("%d",&a[i]);
+	}
+	printf("您输入的数组为:");
+	for(i=1;i<=15;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+	printf("请任意输入要查找的一个整数:");
+	scanf("%d",&n);
+	flag=0;
+	distance=top-bot;
+	for(top=15,bot=1;distance>=1;)
+	{
+	    if(n>a[bot+distance/2])
+		{
+            top=bot+distance/2;
+			distance=top-bot;
+		}
+		if(n==a[bot+distance/2])
+		{
+			flag=bot+distance/2;
+		}
+		if(n>a[bot+distance/2])
+		{
+            bot=bot+distance/2;
+			distance=top-bot;
+		}
+	}
+    if(flag!=0)
+	{
+		printf("该数是第%d个元素的值\n",flag);
+	}
+	else
+	{
+		printf("无此数\n");
+	}
+	return 0;
+}
