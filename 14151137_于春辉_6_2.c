b76a4b9abeb17622454de90435c9140f53e9ae25
2015CLanguage <buaa_2015_c@163.com>
2015-04-28 13:50:41 +0800

    Create 14151137_于春辉_6_2.c

---
+++ 14151137_于春辉_6_2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{int i,j,min,t,a[11];
+   printf("enter data:\n");
+   for (i=1;i<=10;i++)
+   {printf("a[%d]=",i);
+    scanf("%d",&a[i]);
+   }
+   printf("\n");
+   printf("原始数据是:\n");
+   for (i=1;i<=10;i++)
+	   printf("%5d",a[i]);
+   printf("\n");
+   for(i=1;i<=9;i++)
+   {min=i;
+   for(j=i+1;j<=10;j++)
+	   if (a[min]>a[j])min=j;
+	   t=a[i];
+	   a[i]=a[min];
+	   a[min]=t;
+   }
+   printf("\n数据排序后是:\n");
+   for(i=1;i<=10;i++)
+	   printf("%5d",a[i]);
+   printf("\n");
+	   return 0;
+}
