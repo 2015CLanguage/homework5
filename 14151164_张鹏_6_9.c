bc88246fa23d98e078938de618b551a70de02b36
2015CLanguage <buaa_2015_c@163.com>
2015-04-30 00:09:45 +0800

    Update 14151164_张鹏_6_9.c

--- 14151164_张鹏_6_9.c
+++ 14151164_张鹏_6_9.c
@@ -17,20 +17,18 @@ int main()
 				printf("%7d",a[i]);
 			printf("\n");
 			printf("输入要查询的数字:");
-	        scanf("%d", &n);
-	        if (n>a[7])
-	         {
-		    for (i = 6; i >= 0; i--)
-		     if (n == a[i])break;
-             }
-	         else
-	         {
-		      for (i = 7; i < 15; i++)
-		      if (n == a[i])break;
-	         }
-	          if (i == -1 || i == 15)printf("查无此数\n");
-	          else printf("%d为第%d个数\n", n, i + 1);
-	          return 0;
+	                scanf("%d", &n);
+	                if (n>a[7])
+	                {
+		         for (i = 6; i >= 0; i--)
+		         if (n == a[i])break;
+                        }
+	                else
+	                {
+		         for (i = 7; i < 15; i++)
+		         if (n == a[i])break;
+	                } 
+	                 printf("%d为第%d个数\n", n, i + 1);
+	                 return 0;
 }
-
 
