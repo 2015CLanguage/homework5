ed3e897751d9806cb4c343373a962b53bfd09fe5
2015CLanguage <buaa_2015_c@163.com>
2015-04-29 23:58:55 +0800

    Update 14151164_张鹏_6_9.c

--- 14151164_张鹏_6_9.c
+++ 14151164_张鹏_6_9.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int a[15],i,b,c,h,d=0,e=14,t;
+	int a[15],i,b,c,n;
 	for(i=0;i<=14;i++)
 		scanf("%d",&a[i]);
 	printf("\n");
@@ -16,30 +16,21 @@ int main()
 			for(i=0;i<15;i++)
 				printf("%7d",a[i]);
 			printf("\n");
-			printf("请输入一个数");
-			scanf("%d",&h);
-			if ((h>a[0])||(h<a[14]))
-				printf("无法找到\n");
-            else {
-        for(i=1;;i++){
-		    t=(d+e)/2;
-			if (h==a[t]){
-			    printf("%d\n",t+1);
-                break;
-			}
-			else {
-			    if (h<a[t])
-				    d=t+1;
-				else
-					e=t-1;
-			}
-			if (e<=d){
-			    printf("找不到 %d\n",h);
-                break;
-			}
-
-			}
-		}
-	return 0;
+			printf("输入要查询的数字:");
+	        scanf("%d", &n);
+	        if (n>a[7])
+	         {
+		    for (i = 6; i >= 0; i--)
+		     if (n == a[i])break;
+             }
+	         else
+	         {
+		      for (i = 7; i < 15; i++)
+		      if (n == a[i])break;
+	         }
+	          if (i == -1 || i == 15)printf("查无此数\n");
+	          else printf("%d为第%d个数\n", n, i + 1);
+	          return 0;
 }
+
 
