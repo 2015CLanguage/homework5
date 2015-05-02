74c3b8d0c6674f690462c7e1cbf2f2e7fcc63924
2015CLanguage <buaa_2015_c@163.com>
2015-05-02 22:03:32 +0800

    Create 14151177_唐勇_6_2.c

---
+++ 14151177_唐勇_6_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,t,a[10];
+	printf("please input ten int numbers：\n");
+	for (i=0;i<10;i++)
+		scanf("%d",&a[i]);
+	printf("\n");
+	for (j=0;j<9;j++)
+		for(i=0;i<9-j;i++)
+			if (a[i]>a[i+1])
+			{t=a[i];a[i]=a[i+1];a[i+1]=t;}
+	for (i=0;i<10;i++)
+			printf("%d",a[i]);
+	printf("\n");
+	return 0;
+}
