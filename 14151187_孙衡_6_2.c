446d141290f71c231e770f82a98ecc66500d5013
2015CLanguage <buaa_2015_c@163.com>
2015-05-02 23:37:17 +0800

    Create 14151187_孙衡_6_2.c

---
+++ 14151187_孙衡_6_2.c
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+int main() 
+{ 
+	int i,j,a[11],min,x; 
+	printf("依次输入十个整数：\n"); 
+	for(i=1;i<=10;i++) 
+	{ 
+		printf("a[%d]=",i); 
+		scanf("%d",&a[i]); 
+	} 
+	printf("\n"); 
+	for(i=1;i<=9;i++) 
+	{ 
+		min=i; 
+		for(j=i+1;j<=10;j++) 
+			if(a[min]>a[j]) 
+			min=j; 
+		  x=a[i]; 
+		  a[i]=a[min]; 
+			a[min]=x; 
+	} 
+	printf("这十个数由小到大排序为：\n"); 
+	for(i=1;i<=10;i++) 
+		printf("%d\n",a[i]); 
+ 	return 0; 
+} 
