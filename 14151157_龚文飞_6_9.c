f747754177a4bb47025b612ea8425d8086eb0d7c
2015CLanguage <buaa_2015_c@163.com>
2015-05-02 16:40:26 +0800

    Update 14151157_龚文飞_6_9.c

--- 14151157_龚文飞_6_9.c
+++ 14151157_龚文飞_6_9.c
@@ -1,30 +1,47 @@
 #include<stdio.h>
 void main()
 {
-	int a[15],i,j=7,k=4,n;
-	printf("请按由大到小的顺序输入十五个整数\n");
-	for(i=0;i<15;i++)         
-		scanf("%d",&a[i]);
-	printf("输入的数组为\n");
-	for(i=0;i<15;i++)         
-		printf("%3d",a[i]);
-	printf("\n");
-	printf("请输入所要查询数字\n");
-	scanf("%d",&n);
-	while(k>0)   
+	printf("请输入15个数：\n");
+	int a[16], i, j, max, x = 0, sign, low, high, mid, n;
+	for (i = 1; i <= 15; i++)
+		scanf("%d", &a[i]);
+	for (i = 1; i <= 14; i++)
 	{
-		if(n<a[j])
-			j=j+k;
-		else
-			j=j-k;
-		k=k-2;
+		max = i;
+		for (j = i + 1; j <= 15; j++)
+		if (a[max] < a[j])
+			max = j;
+		x = a[i];
+		a[i] = a[max];
+		a[max] = x;
 	}
-	if(n==a[j])   //遍历这三个数字，确定其位置
-		printf("该元素为第%d个元素\n",j+1);
-	else if(n==a[j+1])
-		printf("该元素为第%d个元素\n",j+2);
-	else if(n==a[j-1])
-		printf("该元素为第%d个元素\n",j);
+	printf("这十五个数由大到小排列为：");
+	for (i=1;i<=15;i++)
+		printf("%d,",a[i]);
+	printf("\n请输入要查找的数：");
+	scanf("%d", &x);
+	sign = 0;
+	low = 1;
+	high = 15;
+	if (x>a[1]&&x<a[15])
+	{
+		n=-1;
+	}
 	else
-		printf("不存在该数字\n");
+		while(sign==0&&low<=high)
+		{
+			mid=(low+high)/2;
+			if(x==a[mid])
+			{
+				n=mid;
+				sign=1;
+				printf("数字%d,在数组中位数是：%d\n",x,n);
+			}
+			else if (x>a[mid])
+				high=mid-1;
+			else
+				low=mid+1;
+		}
+	if (sign==0&&n==-1)
+		printf("找不到这个数\n");
 }
