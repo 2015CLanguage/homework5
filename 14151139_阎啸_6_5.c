a39ec59608ec1bfe0f1e540144a1f85fbd4b01a0
2015CLanguage <buaa_2015_c@163.com>
2015-05-04 14:37:55 +0800

    Create 14151139_阎啸_6_5.c

---
+++ 14151139_阎啸_6_5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+      int i,j,k,t,a[5];
+      printf("请输入五个整数:\n");
+	  for(i=0;i<5;i++)
+		  scanf("%d",&a[i]);
+	  printf("\n");
+	  for(i=0;i<2;i++)
+		  t=a[i],a[i]=a[4-i],a[4-i]=t;
+      printf("逆序为:\n");
+	  for(i=0;i<5;i++)
+		  printf("%d ",a[i]);
+	  printf("\n");
+
+}
