99c20a788e6615d21e1413252a1ec3195d831cd7
2015CLanguage <buaa_2015_c@163.com>
2015-05-10 12:58:02 +0800

    Create 14151198_高威_6-5.c

---
+++ 14151198_高威_6-5.c
@@ -0,0 +1,15 @@
+Enter file contents here#include <stdio.h>
+int a[10];
+int n;
+int main()
+{
+	int i,j;
+	scanf("%d",&n);
+	for (i=0; i<n; i++)  scanf("%d",a+i);
+	for (i=0; i<n/2; i++)
+	{
+		int t=a[i];a[i]=a[n-1-i];a[n-i-1]=t;
+	}
+	for (i=0; i<n; i++)  printf("%d ",a[i]);printf("\n");
+	return 0;
+}
